0c9830370dbccaccca6d0abd5c4d55278ba6ffaa
Faisal Wafa <faisalwafa10@gmail.com>
2020-01-14 09:03:23 +0700

    add connection

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+        s
+        </div>
+        <div class="col-md-4">
+        s
+        </div>
+    </div>
+</div>
 

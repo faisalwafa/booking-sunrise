03c35154a131cb7a490a2cdd4e86459ecce7b358
aldisuryap <appleimport123@gmail.com>
2020-01-15 15:27:55 +0700

    Add footer

---
+++ pages/tour/tour.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once '../inc/header.php'; ?>
+    <title>Autentikasi | Sunrise Indonesia</title>
+</head>
+
+<body>
+    <?php include_once '../inc/footer.php'; ?>
+    <?php include_once '../inc/scripts.php'; ?>
+</body>
+
+</html>

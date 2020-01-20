ecafc7b504336988dd345134d9618c143ad0fc51
aldisuryap <appleimport123@gmail.com>
2020-01-20 18:57:56 +0700

    Make Responsive

--- pages/inc/navbar.php
+++ pages/inc/navbar.php
@@ -1,19 +1,27 @@
-<nav class="navbar navbar-expand-lg navbar-custom">
-    <div class="container">
-        <a class="navbar-brand" href="#">
-            <img src="/booking-sunrise/assets/logo.jpg" alt="logo" class="navbar-img">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSunrise" aria-controls="navbarSunrise" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSunrise">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link text-white mr-3" href="#">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Register</a>
-                </li>
+<div class="main-header">
+    <nav class="navbar navbar-expand-lg navbar-custom">
+        <div class="container">
+            <a class="navbar-brand" href="https://sunrise-indonesia.com/">
+                <img src="/booking-sunrise/assets/logo.jpg" alt="logo" class="navbar-img">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSunrise" aria-controls="navbarSunrise" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse item-list" id="navbarSunrise">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link mr-3" href="https://sunrise-indonesia.com/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mr-3" href="https://sunrise-indonesia.com/info-wisata-indonesia/">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mr-3" href="#">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Register</a>
+                    </li>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</div>

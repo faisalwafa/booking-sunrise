36b7cc0c6a6a37faa2525e06dcc28993c77e3a98
aldisuryap <appleimport123@gmail.com>
2020-01-21 11:39:05 +0700

    Add profile but with bug

--- pages/inc/header.php
+++ pages/inc/header.php
@@ -9,6 +9,7 @@
 <link rel="stylesheet" type="text/css" href="../../css/navbar.css">
 <link rel="stylesheet" type="text/css" href="../../css/animate.css">
 <link rel="stylesheet" type="text/css" href="../../css/responsive.css">
+<link rel="stylesheet" type="text/css" href="../../css/profile.css">
 <link rel="stylesheet" type="text/css" href="../../css/font-awesome.css">
 <link rel="stylesheet" type="text/css" href="../../css/custom-bootstrap.css">
 <link rel="stylesheet" type="text/css" href="../../css/trumbowyg.min.css">
---
+++ pages/profile/profile.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once '../inc/header.php'; ?>
+    <title>Tours | Sunrise Indonesia</title>
+</head>
+
+<body>
+    <?php
+    include_once '../inc/navbar.php'; ?>
+
+    <div class="container p-4 p-md-5 pt-5 ">
+        <div class="content">
+            <div class="d-flex align-items-stretch">
+                <nav class="navbar-expand-lg sidebar">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarProfile" aria-controls="navbarProfile" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarProfile">
+                        <ul class="list-unstyled components mb-5">
+                            <li class="active">
+                                <h5>Akun Saya</h5>
+                            </li>
+                            <li class="active">
+                                <a href="#"><span class="fa fa-home mr-3"></span> Home</a>
+                            </li>
+                            <li>
+                                <a href="#"><span class="fa fa-download mr-3 notif"><small class="d-flex align-items-center justify-content-center">5</small></span> Download</a>
+                            </li>
+                            <li>
+                                <a href="#"><span class="fa fa-gift mr-3"></span> Gift Code</a>
+                            </li>
+                            <li>
+                                <a href="#"><span class="fa fa-trophy mr-3"></span> Top Review</a>
+                            </li>
+                            <li>
+                                <a href="#"><span class="fa fa-cog mr-3"></span> Settings</a>
+                            </li>
+                            <li>
+                                <a href="#"><span class="fa fa-support mr-3"></span> Support</a>
+                            </li>
+                            <li>
+                                <a href="#"><span class="fa fa-sign-out mr-3"></span> Sign Out</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </nav>
+                <div id="content" class="pl-5">
+                    <h2 class="mb-4">Sidebar #09</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+
+
+
+
+    <?php include_once '../inc/footer.php'; ?>
+    <?php include_once '../inc/scripts.php'; ?>
+</body>
+
+</html>

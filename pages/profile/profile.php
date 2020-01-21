d4c889ec3d940ab8ff094527de2ddb318e1b2b80
aldisuryap <appleimport123@gmail.com>
2020-01-21 19:49:52 +0700

    add sidebar in profile

--- pages/profile/profile.php
+++ pages/profile/profile.php
@@ -10,7 +10,45 @@
     <?php
     include_once '../inc/navbar.php'; ?>
 
-    <div class="container p-4 p-md-5 pt-5 ">
+    <div class="container pt-4">
+        <div class="content wrapper d-flex align-items-stretch">
+            <nav id="sidebar">
+                <div class="custom-menu">
+                    <button type="button" id="sidebarCollapse" class="d-sm-block d-md-none btn btn-primary">
+                    </button>
+                </div>
+
+                <ul class="list-unstyled components mb-5">
+                    <li>
+                        <h4>Akun Saya</h4>
+                    </li>
+                    <li class="active">
+                        <a href="#"><span class="fa fa-home mr-3"></span> Profile</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="fa fa-gift mr-3"></span> Riwayat Booking</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="fa fa-sign-out mr-3"></span> Sign Out</a>
+                    </li>
+                </ul>
+
+            </nav>
+            <div id="content" class="p-4 p-md-5 pt-5">
+                <h2 class="mb-4">Sidebar #09</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+    <!-- <div class="container pt-5 ">
         <div class="content">
             <div class="d-flex align-items-stretch">
                 <nav class="navbar-expand-lg sidebar">
@@ -58,7 +96,7 @@
         </div>
 
 
-    </div>
+    </div> -->
 
 
 

5c773744dff0dee73c3fe4b11d58462bc2342243
Daffa Akbar <daffaputradamar@gmail.com>
2020-01-21 08:32:46 +0700

    add sidebar why us?

---
+++ pages/inc/sidebar.php
@@ -0,0 +1,71 @@
+<?php
+$sql_last_minute_deals = "SELECT ID, post_";
+?>
+<div class=" content p-2">
+    <h6 class="p-1">Last Minute Deals</h6>
+    <div class="row px-2">
+        <div class="col-md-5">
+            <img src="https://dummyimage.com/600x400/000/fff&text=dummy_image" class="img-fluid" alt="Responsive image">
+        </div>
+        <div>
+            <p>tes</p>
+        </div>
+    </div>
+    <hr>
+    <div class="row px-2">
+        <div class="col-md-5">
+            <img src="https://dummyimage.com/600x400/000/fff&text=dummy_image" class="img-fluid" alt="Responsive image">
+        </div>
+        <div>
+            <p>tes</p>
+        </div>
+    </div>
+    <hr>
+    <div class="row px-2">
+        <div class="col-md-5">
+            <img src="https://dummyimage.com/600x400/000/fff&text=dummy_image" class="img-fluid" alt="Responsive image">
+        </div>
+        <div>
+            <p>tes</p>
+        </div>
+    </div>
+</div>
+<div class=" content mt-4 p-2">
+    <h6 class="p-1">Kenapa Booking Melalui Kami?</h6>
+    <br>
+    <div class="px-2 d-flex align-items-start">
+        <div class="mr-2">
+            <i class="soap-icon-hotel-1 circle sidebar-icon"></i>
+        </div>
+        <div>
+            <h6 class="sidebar-li-title">200+ Hotels</h6>
+            <span>200+ Jaringan Hotel Di Seluruh Indonesia.</span>
+        </div>
+    </div>
+    <hr>
+    <div class="px-2 d-flex align-items-start">
+        <div class="mr-2">
+            <i class="soap-icon-savings circle sidebar-icon"></i>
+        </div>
+        <div>
+            <h6 class="sidebar-li-title">Low Rates & Savings</h6>
+            <span>Garansi Harga Paling Murah.</span>
+        </div>
+    </div>
+    <hr>
+    <div class="px-2 d-flex align-items-start mb-2">
+        <div class="mr-2">
+            <i class="soap-icon-support circle sidebar-icon"></i>
+        </div>
+        <div>
+            <h6 class="sidebar-li-title">Excellent Support</h6>
+            <span>Customer Service 24/7.</span>
+        </div>
+    </div>
+</div>
+<div class=" content mt-4 p-2">
+    <h6 class="p-1">Butuh Bantuan Kami ?</h6>
+    <p>Teman Setia Perjalanan Wisata Anda, dengan senang hati kami melayani anda dengan support customer service 24/7</p>
+    <h4 class="contact p-2"> +6287 777 890 888</h4>
+    <a class="email" href="mailto:info@sunrise-indonesia.com">info@sunrise-indonesia.com</a>
+</div>
